7b15878250538d476f1e696cef2600f1cd77fbf0
basikhs <vaskingvas@gmail.com>
2023-05-17 19:51:05 +0300

    Changes to rock/paper/scissors.

--- commands/misc.py
+++ commands/misc.py
@@ -41,15 +41,25 @@ class Misc(commands.Cog):
         description = "Plays rock-paper-scissors with you."
     )
     async def rps(self, ctx):
-        await ctx.send("To play type rock/paper/scissors!")
+        choices = ['rock', 'paper', 'scissors']
+        await ctx.send("To play, please type rock/paper/scissors or exit to exit the game!")
 
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel
+
+        while True:
+            message = await self.bot.wait_for('message', check = check)
+            player_choice = message.content.lower()
+
+            if player_choice == 'exit':
+                await ctx.send("Exiting the game!")
+                return
+            
+            if player_choice in choices:
+                break
+
+            await ctx.send("Please type rock/paper/scissors or exit to exit the game!")
         
-        message = await self.bot.wait_for('message', check = check)
-        
-        player_choice = message.content.lower()
-        choices = ['Rock', 'Paper', 'Scissors']
         computer_choice = random.choice(choices).lower()
 
         if player_choice == computer_choice:
