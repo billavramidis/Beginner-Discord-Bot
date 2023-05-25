fbbf5a3816525805d0a18f451bd9f4f58cbc10e1
basikhs <vaskingvas@gmail.com>
2023-05-25 20:14:01 +0300

    Separated rps and tictactoe to different files

--- cogs/commands/games/rps.py
+++ cogs/commands/games/rps.py
@@ -1,7 +1,7 @@
 from disnake.ext import commands
 import random
 
-class Games(commands.Cog):
+class Rps(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
@@ -40,30 +40,5 @@ class Games(commands.Cog):
         else:
             await ctx.send(f"`I win! I picked {computer_choice}.`")
 
-    @commands.slash_command(
-        name = "tictactoe",
-        description = "You play Tic-Tac-Toe with someone."
-    )
-    async def tictactoe(self, ctx):
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-        
-        await ctx.send("Welcome to Tic-Tac-Toe! To play mention an other server member!")
-        message = await self.bot.wait_for("message", check=check)
-        
-        while True:
-            if message.mentions:
-                mentioned_user = message.mentions[0]
-                original_user = ctx.user
-                if mentioned_user == original_user:
-                    await ctx.send("You can't mention yourself! Mention someone else to continue!")
-                    message = await self.bot.wait_for("message", check=check)
-                else:
-                    print("Success!!")
-                    break
-            else:
-                await ctx.send("You need to mention someone to continue!")
-                message = await self.bot.wait_for("message", check=check)
-
 def setup(bot):
-    bot.add_cog(Games(bot))
+    bot.add_cog(Rps(bot))
