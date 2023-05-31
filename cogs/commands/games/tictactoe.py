4789aaa67b0c16a92bf8d65051d850b11cd588c8
basikhs <vaskingvas@gmail.com>
2023-05-31 20:56:11 +0300

    Continued working on tic-tac-toe

--- cogs/commands/games/tictactoe.py
+++ cogs/commands/games/tictactoe.py
@@ -9,25 +9,34 @@ class TicTacToe(commands.Cog):
         description = "You play Tic-Tac-Toe with someone."
     )
     async def tictactoe(self, ctx):
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
+        
+        channel = ctx.channel
+
+        print(channel)
+        
+        def check_first(first):
+            return first.author == ctx.author and first.channel == channel
         
         await ctx.send("Welcome to Tic-Tac-Toe! To play mention an other server member!")
-        message = await self.bot.wait_for("message", check=check)
+        message = await self.bot.wait_for("message", check=check_first)
         
         while True:
             if message.mentions:
-                mentioned_user = message.mentions[0]
-                original_user = ctx.user
-                if mentioned_user == original_user:
-                    await ctx.send("You can't mention yourself! Mention someone else to continue!")
-                    message = await self.bot.wait_for("message", check=check)
+                first_player = ctx.user
+                second_player = message.mentions[0]
+                if first_player == second_player:
+                    await ctx.send("You can't mention yourself! Mention someone else to continue or type 'exit' to exit!")
+                    message = await self.bot.wait_for("message", check=check_first)
                 else:
-                    print("Success!!")
+                    await ctx.send(f"{second_player.mention} do you want to play with {first_player.mention}?")
+                    print("Success!!!!")
                     break
+            elif message.content.lower() == 'exit':
+                await ctx.send("You have successfully exited the game!")
+                break
             else:
-                await ctx.send("You need to mention someone to continue!")
-                message = await self.bot.wait_for("message", check=check)
+                await ctx.send("You need to mention someone to continue or type 'exit' to exit!")
+                message = await self.bot.wait_for("message", check=check_first)
 
 def setup(bot):
     bot.add_cog(TicTacToe(bot))
